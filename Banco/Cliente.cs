60455bd60e37ee07622492ea2fb4b4781ccdc8bf
Luiz Fantin <fantin_luiz@outlook.com>
2020-12-30 16:42:47 -0300

    Projeto Banco

---
+++ Banco/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    public class Cliente
+    {
+        public string nome;
+        public string profissao;
+        public string Cpf { get; set; }
+
+        public Cliente(string nome, string cpf, string profissao)
+        {
+            this.nome = nome;
+            this.Cpf = cpf;
+            this.profissao = profissao;
+        }
+
+
+
+    }
+}
---
+++ Banco/Conta.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banco
+{
+    public class Conta
+    {
+        public static int TotalDeContasCriadas { get; set; }
+
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double Saldo { get; set; }
+
+        public Conta(Cliente titular, int agencia, int numero, double saldo)
+        {
+            this.titular = titular;
+            this.agencia = agencia;
+            this.numero = numero;
+            Saldo = saldo;
+        }
+
+      
+
+        public bool Sacar(double valor)
+        {
+            Saldo -= valor;
+            return true;
+        }
+    }
+}
